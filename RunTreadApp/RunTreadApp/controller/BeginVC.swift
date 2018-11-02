76bd3f46a1ac55d340fd41798281760748a205e9
Kumar Chikkanna <kumar.c@xlbyte.com>
2018-11-02 15:13:44 +0530

    beginVC added

--- RunTreadApp/RunTreadApp/controller/BeginVC.swift
+++ RunTreadApp/RunTreadApp/controller/BeginVC.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  RunTreadApp
 //
 //  Created by Apple on 02/11/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class BeginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- RunTreadApp/RunTreadApp/controller/RunLogVC.swift
+++ RunTreadApp/RunTreadApp/controller/RunLogVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  RunTreadApp
 //
 //  Created by Apple on 02/11/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class RunLogVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
