54d0c7d7b4ccd23c878bb64b8bca56be58d47abd
Kumar Chikkanna <kumar.c@xlbyte.com>
2018-11-02 14:39:51 +0530

    added gitignore file

---
+++ RunTreadApp/RunTreadApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RunTreadApp
+//
+//  Created by Apple on 02/11/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RunTreadApp/RunTreadApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RunTreadApp
+//
+//  Created by Apple on 02/11/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
