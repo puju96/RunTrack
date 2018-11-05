d8764755c9ec1cbc557d4cff650351fded1cc3b5
Kumar Chikkanna <kumar.c@xlbyte.com>
2018-11-05 11:53:53 +0530

    map setting is done

--- RunTreadApp/RunTreadApp/controller/BeginVC.swift
+++ RunTreadApp/RunTreadApp/controller/BeginVC.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import MapKit
+class BeginVC: LocationVC{
 
-class BeginVC: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       checkUserAuthLocation()
+        mapView.delegate = self
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.startUpdatingLocation()
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        manager?.stopUpdatingLocation()
     }
 
+    @IBAction func centerLocationBtnTapped(_ sender: Any) {
+    }
+    
+    
+}
 
+extension BeginVC : CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse{
+            checkUserAuthLocation()
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+        }
+    }
+    
 }
 
---
+++ RunTreadApp/RunTreadApp/controller/LocationVC.swift
@@ -0,0 +1,32 @@
+//
+//  LocationVC.swift
+//  RunTreadApp
+//
+//  Created by Apple on 05/11/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class LocationVC: UIViewController , MKMapViewDelegate{
+    
+    var manager : CLLocationManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+       
+    }
+    
+    func checkUserAuthLocation(){
+        if  CLLocationManager.authorizationStatus() != .authorizedWhenInUse{
+            manager?.requestWhenInUseAuthorization()
+        }
+        
+    }
+    
+
+   
+}
